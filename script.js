527cfaa19f9bfa0f67f7684e6d495e3f859d347a
Mariel Waters <marielcwaters@gmail.com>
2018-09-02 17:41:29 -0600

    Add unique identifier and start local storage

--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 var saveButton = document.querySelector('.save-button');
 var inputSearch = document.querySelector('.input-search');
 
-
 saveButton.addEventListener('click', createIdeaCard);
+
+function createId() {
+  return 'id-' + Math.random().toString(36).substr(2, 16);
+};
 
 function createIdeaCard() {
   event.preventDefault();
@@ -11,23 +14,27 @@ function createIdeaCard() {
   var inputBody = document.querySelector('.input-body');
   var ideaCardList = document.querySelector('.idea-card-list');
   var newCardContainer = document.querySelector('.new-card-container');
+  var uniqueId = createId();
   var newCard = document.createElement('li');
-  newCard.innerHTML = `<form class="idea-card">
-                            <span class="idea-card-title-and-delete-button">
-                            <h2 class="idea-card-title">${inputTitle.value}</h2>
-                            <img src="FEE-ideabox-icon-assets/delete.svg" class="card-buttons delete-button">
-                            </span>
-                            <p class="idea-card-text">${inputBody.value}
-                            </p>
-                            <span class="vote-buttons-and-quality">
-                            <img src="FEE-ideabox-icon-assets/upvote.svg" class="card-buttons upvote-button">
-                            <img src="FEE-ideabox-icon-assets/downvote.svg" class="card-buttons downvote-button">
-                            <p class="quality">quality:<span class="quality-setting"> swill</span>
-                            </p>
-                            </span>
-                            <hr>
-                            </form>`;
+  newCard.innerHTML = `<form class="idea-card" data=${uniqueId}>
+                        <span class="idea-card-title-and-delete-button">
+                          <h2 class="idea-card-title">${inputTitle.value}
+                          </h2>
+                          <img src="FEE-ideabox-icon-assets/delete.svg" class="card-buttons delete-button">
+                        </span>
+                        <p class="idea-card-text">${inputBody.value}
+                        </p>
+                        <span class="vote-buttons-and-quality">
+                          <img src="FEE-ideabox-icon-assets/upvote.svg" class="card-buttons upvote-button">
+                          <img src="FEE-ideabox-icon-assets/downvote.svg" class="card-buttons downvote-button">
+                          <p class="quality">quality:<span class="quality-setting"> swill</span>
+                          </p>
+                        </span>
+                        <hr>
+                      </form>`;
+  localStorage.setItem(uniqueId, JSON.stringify(newCard));
   newCardContainer.prepend(newCard);
   inputTitle.value = '';
   inputBody.value = '';
 };
+
